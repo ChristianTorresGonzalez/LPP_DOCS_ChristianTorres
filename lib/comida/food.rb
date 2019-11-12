c9ffdb6a2219565f2f23b88f2a1ef6b742d78f89
Christian Torres Gonzalez <christiantorres8952@gmail.com>
2019-11-12 08:33:44 +0000

    Acierto prueba get_gei() para atributo gei

--- lib/comida/food.rb
+++ lib/comida/food.rb
@@ -1,7 +1,7 @@
 module Comida
 	class Comida
 		
-		attr_reader :nombre, :proteina, :carbohidratos, :lipidos
+		attr_reader :nombre, :proteina, :carbohidratos, :lipidos, :gei
 		
 		def initialize
 		
